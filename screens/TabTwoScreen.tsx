9e01c5f48b6a27c8c2c247b6977a9ce1433cf2ce
cristiandev <cristiansotomayor.dev@gmail.com>
2021-05-08 22:19:17 -0500

    cosas

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,14 +1,41 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function TabTwoScreen() {
+  const data = [{
+    text: 'hola0'
+  }, {
+    text: 'hola1'
+  }, {
+    text: 'hol3'
+  }];
+
+  const onPressLearnMore = (data: any) => {
+    console.log('pressed', data);
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+
+      {
+        data.map(item => (
+          <View>
+            <Text key={item.text}>{ item.text }</Text>
+            <Button
+              onPress={() => onPressLearnMore(item)}
+              title="Learn More"
+              color="#841584"
+              accessibilityLabel="Learn more about this purple button"
+            />
+          </View>
+        ))
+      }
+
       <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
     </View>
   );
