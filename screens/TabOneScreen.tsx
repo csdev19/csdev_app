edc227cb0ad08beb0461ab5e7d1ecc358491a529
cristiandev <cristiansotomayor.dev@gmail.com>
2021-05-08 22:19:14 -0500

    cosas

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,14 +1,38 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { Image, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
+import { Appearance } from 'react-native'
+
+
+let colorLetter = 'white';
+const colorScheme = Appearance.getColorScheme();
+console.log('colorScheme', colorScheme);
+if (colorScheme === 'dark') {
+  colorLetter = 'white';
+  // Use dark color scheme
+} else if(colorScheme === 'light') {
+  // Use normal color scheme
+  colorLetter = 'black';
+
+}
+
+
 export default function TabOneScreen() {
+  const url = 'https://s3.us-east-2.amazonaws.com/csdev19.personal/logo1.png';
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View style={styles.imageContainer}>
+        <Image source={{ uri: url }} style={styles.image} />
+      </View>
+      <View style={styles.titleContainer}>
+        <Text style={styles.subtitle}>¡Hola a todos!</Text>
+        <Text style={styles.title}>Soy Cristian Sotomayor</Text>
+      </View>
+      {/* <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" /> */}
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
@@ -21,12 +45,33 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: colorLetter, //'white',
+    textAlign: 'center',
+  },
+  subtitle: {
     fontSize: 20,
     fontWeight: 'bold',
+    color: colorLetter, //'white',
   },
   separator: {
     marginVertical: 30,
     height: 1,
     width: '80%',
   },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: "contain"
+  },
+  imageContainer: {
+    width: '100%',
+    height: '50%'
+  },
+  titleContainer: {
+    marginBottom: 20,
+    marginTop: 20,
+  }
 });
+
